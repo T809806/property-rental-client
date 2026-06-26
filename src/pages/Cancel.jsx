aee5348de95e62c152ec3e1f1c583b18ead2fe45
T806809 <tahiyaakter94@gmail.com>
2026-06-26 22:59:21 +0600

    fix some issue inside cancel section

--- src/pages/Cancel.jsx
+++ src/pages/Cancel.jsx
@@ -3,32 +3,29 @@ import { useNavigate } from "react-router-dom";
 const Cancel = () => {
   const navigate = useNavigate();
 
-  return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-[#0b0f14] text-white">
-      
-      <h1 className="text-3xl font-bold text-red-500">
-        Payment Cancelled ❌
-      </h1>
+ return (
 
-      <p className="text-gray-400 mt-3">
-        Your payment was not completed.
-      </p>
+ <div className="min-h-screen flex flex-col items-center justify-center bg-[#0b0f14] text-white"> 
+  <h1 className="text-3xl font-bold text-red-500"> Payment Cancelled ❌ </h1>
 
-      <button
-        onClick={() => navigate(-1)}
-        className="mt-6 bg-[#d6b46d] text-black px-6 py-3 rounded font-semibold hover:opacity-90"
-      >
-        Go Back
-      </button>
+ <p className="text-gray-400 mt-3"> Your payment was not completed. </p>
+ <button
+   onClick={() => navigate(-1)}
+   className="mt-6 bg-[#d6b46d] text-black px-6 py-3 rounded font-semibold hover:opacity-90"
+ >
+ Go Back
+   </button>
 
-      <button
-        onClick={() => navigate("/")}
-        className="mt-3 bg-gray-700 text-white px-6 py-3 rounded hover:bg-gray-600"
-      >
-        Go Home
-      </button>
+  <button
+     onClick={() => navigate("/")}
+     className="mt-3 bg-gray-700 text-white px-6 py-3 rounded hover:bg-gray-600"
+  >
+ Go Home
+
+</button>
 
     </div>
+    
   );
 };
 
