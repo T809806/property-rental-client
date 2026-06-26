58266bb392f66830f00208f4a5b4569fd529c68f
T806809 <tahiyaakter94@gmail.com>
2026-06-26 22:13:30 +0600

    some changes inside checkoutform.jsx

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -6,62 +6,60 @@ import api from "../api/axios";
 const CheckoutForm = () => {
   const stripe = useStripe();
   const elements = useElements();
-
   const navigate = useNavigate();
   const location = useLocation();
 
  const { clientSecret, property, bookingData } = location.state || {};
 
-  const [loading, setLoading] = useState(false);
-
-  const handleSubmit = async (e) => {
+const [loading, setLoading] = useState(false);
+ const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (!stripe || !elements) return;
+ if (!stripe || !elements) return;
 
-    setLoading(true);
+setLoading(true);
 
-    const card = elements.getElement(CardElement);
+const card = elements.getElement(CardElement);
 
-    const { paymentIntent, error } =
-      await stripe.confirmCardPayment(clientSecret, {
-        payment_method: {
-          card,
-        },
-      });
+ const { paymentIntent, error } =
+  await stripe.confirmCardPayment(clientSecret, {
+     payment_method: {
+     card,
+ },
+ });
 
-    if (error) {
+ if (error) {
       alert(error.message);
       setLoading(false);
       return;
-    }
+  }
 
-    if (paymentIntent.status === "succeeded") {
+if (paymentIntent.status === "succeeded") {
   const token = localStorage.getItem("token");
   const user = JSON.parse(localStorage.getItem("user"));
 
   try {
 
-    // Save Booking
-    await api.post(
+ await api.post(
       "/bookings",
-      {
+{
         propertyId: property._id,
         moveInDate: bookingData.moveInDate,
         contactNumber: bookingData.contactNumber,
         notes: bookingData.notes,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
+  },
 
-    // Save Transaction
-    await api.post(
-      "/payment/save",
-      {
+ {
+  headers: {
+     Authorization: `Bearer ${token}`,
+  },
+}
+ );
+
+  
+ await api.post(
+  "/payment/save",
+ {
         transactionId: paymentIntent.id,
         property: property._id,
         tenant: user._id,
@@ -75,7 +73,7 @@ const CheckoutForm = () => {
       }
     );
 
-    // Success Page
+    
     navigate("/success", {
       state: {
         property,
@@ -97,21 +95,22 @@ const CheckoutForm = () => {
       onSubmit={handleSubmit}
       className="bg-[#111827] p-8 rounded-xl border border-[#d6b46d] max-w-lg mx-auto"
     >
-      <h2 className="text-2xl text-[#d6b46d] mb-6 font-bold">
-        Stripe Payment
-      </h2>
+ <h2 className="text-2xl text-[#d6b46d] mb-6 font-bold">  Stripe Payment </h2>
 
-      <div className="bg-white rounded p-4">
-        <CardElement />
-      </div>
+ <div className="bg-white rounded p-4">
+ <CardElement />
+ </div>
 
-      <button
-        disabled={!stripe || loading}
-        className="w-full mt-6 bg-[#d6b46d] text-black py-3 rounded-lg font-semibold"
-      >
-        {loading ? "Processing..." : `Pay ৳${property.price}`}
-      </button>
-    </form>
+ <button
+ disabled={!stripe || loading}
+ className="w-full mt-6 bg-[#d6b46d] text-black py-3 rounded-lg font-semibold"
+ >
+ {loading ? "Processing..." : `Pay ৳${property.price}`}
+
+     </button>
+
+ </form>
+ 
   );
 };
 
