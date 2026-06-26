df53e5392b8fc295114d77440582439c066d564c
T806809 <tahiyaakter94@gmail.com>
2026-06-26 22:46:59 +0600

    some changes added inside propertycard

--- src/components/PropertyCard.jsx
+++ src/components/PropertyCard.jsx
@@ -6,51 +6,45 @@ const PropertyCard = ({ item }) => {
   return (
     <div className="group bg-[#0b1220] border border-[#1f2937] rounded-xl p-4 hover:border-[#d6b46d] transition-all duration-300 hover:scale-[1.03] shadow-lg">
 
-      {/* IMAGE */}
-    
-{/* IMAGE */}
 <div className="h-44 rounded-lg mb-4 overflow-hidden">
   <img
     src={
       item.images?.[0] ||
       "https://images.unsplash.com/photo-1560518883-ce09059eeffa?w=900&auto=format&fit=crop&q=80"
-    }
-    alt={item.title}
-    className="w-full h-full object-cover transition-all duration-500 hover:scale-110"
+ }
+ alt={item.title}
+ className="w-full h-full object-cover transition-all duration-500 hover:scale-110"
   />
+
 </div>  
 
-      {/* TITLE */}
-      <h3 className="text-lg font-bold text-white group-hover:text-[#d6b46d] transition">
-        {item.title}
-      </h3>
+ <h3 className="text-lg font-bold text-white group-hover:text-[#d6b46d] transition">
+   {item.title}
 
-      {/* LOCATION */}
-      <p className="text-gray-400 text-sm mt-1">
-        📍 {item.location}
-      </p>
+ </h3>
 
-      {/* TYPE */}
-      <span className="text-xs bg-[#d6b46d] text-black px-3 py-1 rounded-full inline-block mt-3 font-medium">
-        {item.propertyType}
-      </span>
+ <p className="text-gray-400 text-sm mt-1"> 📍 {item.location}  </p>
 
-      {/* PRICE */}
-      <p className="mt-4 text-[#d6b46d] font-bold text-lg">
-        ৳ {item.price}
-        <span className="text-gray-400 text-sm font-normal"> / {item.rentType}</span>
-      </p>
+  <span className="text-xs bg-[#d6b46d] text-black px-3 py-1 rounded-full inline-block mt-3 font-medium">
+   {item.propertyType}
+ </span>
 
-      {/* BUTTON */}
-      <button
-        onClick={() => navigate(`/property/${item._id}`)}
-        className="mt-5 w-full py-2 rounded-lg bg-[#0f2f2f] border border-[#d6b46d] text-[#d6b46d] font-semibold 
+ <p className="mt-4 text-[#d6b46d] font-bold text-lg">
+   ৳ {item.price}
+ <span className="text-gray-400 text-sm font-normal"> / {item.rentType}</span>
+ </p>
+
+ <button
+   onClick={() => navigate(`/property/${item._id}`)}
+   className="mt-5 w-full py-2 rounded-lg bg-[#0f2f2f] border border-[#d6b46d] text-[#d6b46d] font-semibold 
         hover:bg-[#d6b46d] hover:text-black transition-all duration-300 active:scale-95"
-      >
-        View Details →
-      </button>
+ >
+ View Details →
 
-    </div>
+  </button>
+
+ </div>
+ 
   );
 };
 
